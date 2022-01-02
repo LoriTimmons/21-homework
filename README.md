715156a87e3ad4607bb079229f5fb8fb74af8f42
Lori Timmons <lorietimmons@gmail.com>
2022-01-01 23:56:49 -0600

    server error

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Book Search Engine Starter Code
+
+React Router recently upgraded to version 6 (Links to an external site.) which is incompatible with older versions. When following allow with the module lesson, be sure to install React Router version 5 using this command: npm install react-router-dom@5.
+
+To start see 21.1.3
+npm start mongod
+
+## to install Apollo with (Express.js-specific version of the Apollo server library and the GraphQL library,) See 21.1.4
+npm i apollo-server-express graphql -Done 
+
+Ask why my port is not running when in CL but will run if I go to script. Check the main in the script file. 
+
+Once your server is up and running, navigate to localhost:3001/graphql
--- server/server.js
+++ server/server.js
@@ -1,13 +1,42 @@
 const express = require('express');
+// import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+
+// import our typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+const startServer = async () => {
+
+  // Apollo server and schema data
+  const server = new ApolloServer({ 
+    typeDefs, 
+    resolvers, 
+    context: authMiddleware 
+  });
+
+// Apollo server
+await server.start();
+
+// Apollo  middleware
+server.applyMiddleware({ app });
+
+// log where we can go to test our GQL API
+console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+// Start Apollo server
+startServer();
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {

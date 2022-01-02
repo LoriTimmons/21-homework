49748af759a6746d1c41f3036783ebf74abc047d
Lori Timmons <lorietimmons@gmail.com>
2022-01-02 00:49:51 -0600

    updated typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,9 +4,20 @@ const { gql } = require('apollo-server-express');
 // create our typeDefs
 const typeDefs = gql`
   type Query {
-    helloWorld: String
+    me: User
+    users: [User]
+  }
+
+  type Mutation {
+  login(email: String!, password: String!): Auth
+  addUser(username: String!, email: String!, password: String!): Auth
+  saveBook(bookText: String!): User
+  removeBook(bookId: ID):User
   }
 `;
 
 // export the typeDefs
 module.exports = typeDefs;
+
+// for saveBooks: 🚫Look into creating what's known as an input type to handle all of these parameters!)
+
